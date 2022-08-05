5c77ace8c7afd5fd53dbd09318b37c2feb927957
bennettsharon <sbennett@linkedin.com>
2022-08-05 14:01:12 -0400

    Add files via upload

---
+++ Lesson 3 - Create a SA PS.ps1
@@ -0,0 +1,22 @@
+﻿#Create a Storage account
+
+#Connect to Azure
+Connect-AzAccount
+
+#Variables
+$resourceGroup = "SAPS"
+$location = "CanadaEast"
+$storageaccount = "sbennettjuly6sa2"
+
+#Create a resource Group
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+#Create a storage account
+New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name sbennettjuly6sa2 `
+  -Location $location `
+  -SkuName Standard_RAGRS `
+  -Kind StorageV2
+
+#Delete the storage account
+Remove-AzStorageAccount -Name $storageaccount -ResourceGroupName $resourcegroup
---
+++ Lessson 3 - Create a SA CLI.ps1
@@ -0,0 +1,21 @@
+﻿#Create a SA using CLI
+
+#Login into Azure
+az login
+
+#Create a Resource Group
+az group create \
+  --name storage-resource-group \
+  --location eastus
+
+#Create a Storage Account
+az storage account create \
+  --name sbennettjuly6 \
+  --resource-group storage-resource-group \
+  --location eastus \
+  --sku Standard_RAGRS \
+  --kind StorageV2
+
+
+#Delete SA
+az storage account delete --name sbennettjuly6 --resource-group storage-resource-group
