9be4a283e9afdbea05c8f63df604faafc3be7890
bennettsharon <sbennett@linkedin.com>
2022-08-13 09:42:38 -0400

    Add files via upload

---
+++ Lesson 4 - List VMs using CLI.ps1
@@ -0,0 +1,10 @@
+﻿https://docs.microsoft.com/en-us/cli/azure/vm?view=azure-cli-latest
+
+#List all
+az vm list 
+
+#List all in a RG
+az vm list -g MyResourceGroup
+
+#List all in RG with Details
+az vm list -g MyResourceGroup -d
