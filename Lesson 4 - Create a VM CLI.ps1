0a91de9836f09517f05428a6fba747cf701a1839
bennettsharon <sbennett@linkedin.com>
2022-08-13 09:42:17 -0400

    Add files via upload

---
+++ Lesson 4 - Create a VM CLI.ps1
@@ -0,0 +1,24 @@
+﻿#Create a vm CLI
+
+#Create the resource group
+az group create --name clivm --location canadacentral
+
+#create the RG
+az vm create \
+    --resource-group clivm \
+    --name myVMCLI \
+    --image Win2022AzureEditionCore \
+    --public-ip-sku Standard \
+    --admin-username sharon
+
+#Install IIS
+az vm run-command invoke -g clivm -n myVMCLI --command-id RunPowerShellScript --scripts "Install-WindowsFeature -name Web-Server -IncludeManagementTools"
+
+#Open port 80
+az vm open-port --port 80 --resource-group clivm --name myVMCLI
+
+#List all images 
+az vm image list --all
+
+#Delete resources
+az group delete --name clivm
